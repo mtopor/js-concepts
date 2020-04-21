39dee8eb0d2e6dfea366d5a621822fbc95328dee
mt667118 <maksym.topor@broadcom.com>
2020-04-21 12:57:58 +0200

    JS types

---
+++ 01_types.js
@@ -0,0 +1,10 @@
+ //null, undefined, boolean, string, object, symbol
+console.log(typeof 0);
+console.log(typeof true);
+ console.log(typeof 'text');
+ console.log(typeof undefined); // undefined
+ console.log(typeof Math);// object
+ console.log(typeof Symbol('JS'));
+ console.log(typeof null); //object
+ console.log(typeof function () {}); //function
+ console.log(typeof NaN); //number
